7750209207caaa36555b6c730220481d5d635736
cherucole <cheruyotcheruyot@gmail.com>
2018-11-05 12:44:03 +0300

    Now login redirect works

--- sentimental/settings.py
+++ sentimental/settings.py
@@ -165,5 +165,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-LOGIN_REDIRECT_URL = "/index"
-LOGOUT_REDIRECT_URL = "/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
